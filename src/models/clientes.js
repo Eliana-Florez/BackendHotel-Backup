225833dc60149538922a501dd11b461d369dbffd
Eliana-Florez <evflorezc@itc.edu.co>
2021-10-13 22:13:56 -0500

    cambio en model cliente

--- src/models/clientes.js
+++ src/models/clientes.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 let clienteSchema = new Schema(
     {
-        cedula: { type: Number },
         nombres: { type: String, },
         apellidos: { type: String, },
+        cedula: { type: Number },
         ciudadOrigen: { type: String, },
         telefono: { type: Number, },
         email: { type: String, },
